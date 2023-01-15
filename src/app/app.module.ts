23adefe4c2df8e08a57001ab6c202ad93ce8584b
DavidLyu <59672932+David-Lyu@users.noreply.github.com>
2023-01-14 16:10:45 -0800

    added new component for dynamic component injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentInjectionComponent } from './components/content-injection/content-injection.component';
+import { AnchorTagComponent } from './components/anchor-tag/anchor-tag.component';
 
 @NgModule({
-  declarations: [AppComponent, ContentInjectionComponent],
+  declarations: [AppComponent, ContentInjectionComponent, AnchorTagComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
